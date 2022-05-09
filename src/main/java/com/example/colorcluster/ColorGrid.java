a9c1425c2418fc1125c84bce2fdcb697661a1462
Waleed Saleh <waleed_saleh@stu.najah.edu>
2022-05-09 23:56:32 +0300

    Finished the project, fixed some issues, and some refactoring and renaming

--- src/main/java/com/example/colorcluster/ColorGrid.java
+++ src/main/java/com/example/colorcluster/ColorGrid.java
@@ -9,31 +9,63 @@ import java.util.Random;
 
 public class ColorGrid {
 
-    Color[][] colors;
-    int colorInputSize;
-    int radius;
-    float learnRate;
-    ArrayList<Color> arrayList;
-    Random random;
-    int x;
-    int y;
-    int epic ;
+    private Color[][] colors;
+    private int colorInputSize;
+    private int initialRadius;
+    private float  initialLearningRate;
+    private final ArrayList<Color> arrayList;
+    private final Random random;
+    private int x;
+    private int y;
+    private int epoch;
 
+    public Color[][] getColors() {
+        return colors;
+    }
+
+    public int getColorInputSize() {
+        return colorInputSize;
+    }
+
+    public void setColorInputSize(int colorInputSize) {
+        this.colorInputSize = colorInputSize;
+    }
+
+    public void setRadius(int initialRadius) {
+        this.initialRadius = initialRadius;
+    }
+
+    public void setLearnRate(float initialLearningRate) {
+        this.initialLearningRate = initialLearningRate;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public void setEpoch(int epoch) {
+        this.epoch = epoch;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
 
     ColorGrid() {
         this(new Random(), new ArrayList<>());
     }
 
-    ColorGrid(Random random) {
-        this.random = random;
-        this.arrayList = new ArrayList<>();
-    }
-
     ColorGrid(Random random, ArrayList<Color> arrayList) {
         this.random = random;
         this.arrayList = arrayList;
-        System.out.println("object Created ");
-
     }
 
 
@@ -49,7 +81,6 @@ public class ColorGrid {
                 );
             }
         }
-        System.out.println("array Filled");
     }
 
 
@@ -64,53 +95,34 @@ public class ColorGrid {
     }
     void  solve( ){
         boolean changing=true;
-        System.out.println("epic is "+this.epic);
-        // TODO:epoch or stops changing
-//// && this.epic !=0
-
-        int count =0;
-        if(this.epic!=0) {
-            while (this.epic != 0) {
-                for (Color color : this.arrayList) {
-                    Pair<Integer, Integer> bmu = bestMatchingUnit(color);
-                    changing = update(color, bmu);
-                }
-                epic--;
+        for(int i = 0; i<this.epoch; i++){
+            for (Color color : this.arrayList) {
+                Pair<Integer, Integer> bmu = bestMatchingUnit(color);
+                double learnRate=initialLearningRate*equation(i);
+                int radius=(int)(initialRadius*equation(i));
+                if(radius==0)continue;
+                changing = update(color, bmu,learnRate,radius);
 
             }
+            if(!changing)break;
         }
-        else{
-            while(changing) {
-                if (count>100000)
-                    break;
-               System.out.println(count++);
-                for (Color color : this.arrayList) {
-                    Pair<Integer, Integer> bmu = bestMatchingUnit(color);
-                    changing=update(color, bmu);
-                }
-
-            }
-
-        }
-
     }
 
 
     Pair<Integer,Integer> bestMatchingUnit(Color inputColor){
-        double minDist;
-        minDist=Double.MAX_VALUE;
+        double minDist=Double.MAX_VALUE;
         double red;
         double blue;
         double green;
         double distance;
         Pair<Integer,Integer> pos=null;
-        for (int i=1;i<x;i++){
-            for (int j=1;j<y;j++){
+        for (int i=0;i<x;i++){
+            for (int j=0;j<y;j++){
                 red= this.colors[i][j].getRed()-inputColor.getRed();
                 blue= this.colors[i][j].getBlue()-inputColor.getBlue();
                 green= this.colors[i][j].getGreen()-inputColor.getGreen();
-                distance=Math.sqrt(red+blue+green);
-                if(minDist>distance) {
+                distance=Math.sqrt(red*red+blue*blue+green*green);
+                if(distance<minDist) {
                     minDist = distance;
                     pos=new Pair<>(i,j);
                 }
@@ -118,28 +130,27 @@ public class ColorGrid {
         }
         return pos;
     }
-    boolean update(Color inputColor,Pair<Integer,Integer> pos){
+    boolean update(Color inputColor,Pair<Integer,Integer> pos,double learnRate,int radius){
         boolean change=false;
         for (int i=0;i<x;i++){
             for (int j=0;j<y;j++) {
                 int distance = (i - pos.getKey()) * (i - pos.getKey()) + (j - pos.getValue()) * (j - pos.getValue());//pos
-                if(distance <=radius*radius){
-                    double learnAndW=learnRate*weight(distance);
-                    double red=colors[i][j].getRed()+learnAndW*(inputColor.getRed()-colors[i][j].getRed());
-                    double green=colors[i][j].getGreen()+learnAndW*(inputColor.getGreen()-colors[i][j].getGreen());
-                    double blue=colors[i][j].getBlue()+learnAndW*(inputColor.getBlue()-colors[i][j].getBlue());
-                    Color nColor=Color.color(red,green,blue);
-                    change= change || !Objects.equals(nColor, colors[i][j]);
-                    colors[i][j]=Color.color(red,green,blue);
-
-                }
+                double learnAndW=learnRate*weight(distance,radius);
+                double red=colors[i][j].getRed()+learnAndW*(inputColor.getRed()-colors[i][j].getRed());
+                double green=colors[i][j].getGreen()+learnAndW*(inputColor.getGreen()-colors[i][j].getGreen());
+                double blue=colors[i][j].getBlue()+learnAndW*(inputColor.getBlue()-colors[i][j].getBlue());
+                Color nColor=Color.color(red,green,blue);
+                change= change || !Objects.equals(nColor, colors[i][j]);
+                colors[i][j]=Color.color(red,green,blue);
             }
         }
         return change;
     }
+    double equation(int currentEpoch){
+        return Math.exp(-currentEpoch*(Math.log((double)x*y)/epoch));
+    }
 
-
-    double weight(double d ){
+    double weight(double d ,int radius){
         return  Math.exp(-(d/(2*Math.pow(radius,2))));
     }
 
