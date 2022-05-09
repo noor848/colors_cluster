6d20cde3d86c10a823fe2281d5aad365781a2b1c
Waleed Saleh <waleed_saleh@stu.najah.edu>
2022-05-09 23:54:49 +0300

    Refactoring and Renaming

--- src/main/java/com/example/colorcluster/Gui.java
+++ src/main/java/com/example/colorcluster/Gui.java
@@ -1,6 +1,5 @@
 package com.example.colorcluster;
 
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
@@ -14,12 +13,12 @@ import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Text;
 
 public class Gui {
-
-    public Button start;
-    public TextField epic;
-    ColorGrid grid;
-    int input;
-    Alert warningAlert;
+    public static final String ALERT = "Alert";
+    @FXML
+    private TextField epoch;
+    private ColorGrid grid;
+    private int input;
+    private Alert warningAlert;
     @FXML
     private TextField yField;
     @FXML
@@ -52,139 +51,122 @@ public class Gui {
     @FXML
     private void initialize() {
         warningAlert = new Alert(AlertType.WARNING);
-        warningAlert.setTitle("Alert");
+        warningAlert.setTitle(ALERT);
         grid = new ColorGrid();
         chooseColors.setDisable(true);
         addInputColor.setDisable(true);
         learnRadius.setDisable(true);
-
     }
 
     @FXML
     private void buildGrid() {
-        if (!xField.getText().isBlank() && !yField.getText().isBlank()) {
-            int x = Integer.parseInt(xField.getText());
-            int y = Integer.parseInt(yField.getText());
-            System.out.println(x + "," + y);
-            if (10 <= y && y <= 50 && 10 <= x && x <= 50) {
-                pane.getChildren().clear();
-                pane.setPrefSize(x, y);
-                addInputColor.setDisable(false);
-                inputColor.setDisable(false);
-                chooseColors.setDisable(true);
-                learnRadius.setDisable(true);
-                grid.fillGrid(x, y);
-                System.out.println("Length is " + grid.colors.length);
-                grid.x=x;
-                grid.y=y;
-                for (int i = 0; i < x; i++)
-                    for (int j = 0; j < y; j++)
-                        pane.add(new Circle(0, 0, 4, grid.colors[i][j]), j, i); // c
-
-            } else {
-                warningAlert.setHeaderText("Dimensions should be less than or equal 50 !");
-                warningAlert.showAndWait();
-            }
-        } else {
+        if (xField.getText().isBlank() || yField.getText().isBlank()) {
             warningAlert.setHeaderText("You have to Fill Dimensions !");
             warningAlert.showAndWait();
-
+            return;
         }
+        int x = Integer.parseInt(xField.getText());
+        int y = Integer.parseInt(yField.getText());
+        if (10 > y || y > 50 || 10 > x || x > 50) {
+            warningAlert.setHeaderText("Dimensions should be less than or equal 50 !");
+            warningAlert.showAndWait();
+            return;
+        }
+        pane.getChildren().clear();
+        pane.setPrefSize(x, y);
+        addInputColor.setDisable(false);
+        inputColor.setDisable(false);
+        chooseColors.setDisable(true);
+        learnRadius.setDisable(true);
+        grid.fillGrid(x, y);
+        grid.setX(x);
+        grid.setY(y);
+        for (int i = 0; i < x; i++) {
+            for (int j = 0; j < y; j++) {
+                pane.add(new Circle(0, 0, 4, grid.getColors()[i][j]), j, i); // c
+            }
+        }
+
     }
 
     @FXML
     private void sliderMove() {
         rect.setFill(Color.rgb((int) red.getValue(), (int) green.getValue(), (int) blue.getValue()));
         txtRbg.setText("RGB (" + (int) red.getValue() + "," + (int) green.getValue() + "," + (int) blue.getValue() + ")");
-
-
     }
 
     @FXML
     private void addInputColor() {
-        if (!inputColor.getText().isBlank()) {
-            grid.colorInputSize = Integer.parseInt(inputColor.getText().trim());
-            warningAlert.setHeaderText("YOu added" + grid.colorInputSize + "Inputs");
-            warningAlert.showAndWait();
-            inputColor.setDisable(true);
-            addInputColor.setDisable(true);
-            chooseColors.setDisable(false);
-
-            input = grid.colorInputSize;
-        } else {
+        if (inputColor.getText().isBlank()) {
             warningAlert.setHeaderText("Enter Number of input Colors");
             warningAlert.showAndWait();
-
+            return;
         }
-
-
+        grid.setColorInputSize(Integer.parseInt(inputColor.getText().trim()));
+        warningAlert.setHeaderText("You added " + grid.getColorInputSize() + " inputs");
+        warningAlert.showAndWait();
+        inputColor.setDisable(true);
+        addInputColor.setDisable(true);
+        chooseColors.setDisable(false);
+        input = grid.getColorInputSize();
     }
 
     @FXML
     private void chooseColor() {
-        if (grid.colorInputSize > 0) {
+        int inputSize=grid.getColorInputSize();
+        if (inputSize > 0) {
             grid.insertColor((int) red.getValue(), (int) green.getValue(), (int) blue.getValue());
-            grid.colorInputSize--;
+            inputSize--;
             Alert alert = new Alert(AlertType.INFORMATION);
-            alert.setTitle("Alert");
-            alert.setHeaderText("Added  ");
+            alert.setTitle(ALERT);
+            alert.setHeaderText("Added");
             alert.showAndWait();
-            inputColor.setText(Integer.toString(grid.colorInputSize));
+            inputColor.setText(Integer.toString(inputSize));
         } else {
             warningAlert.setHeaderText("you choose only " + input + "inputs ");
             warningAlert.showAndWait();
         }
-        if (grid.colorInputSize == 0) {
+        if (inputSize == 0) {
             learnRadius.setDisable(false);
         }
-
+        grid.setColorInputSize(inputSize);
     }
 
     @FXML
     private void learnRadius() {
-        if (!learnR.getText().isBlank() && !radius.getText().isBlank()) {
-
-            if (Integer.parseInt(radius.getText()) != 0 ) {
-                grid.epic = !epic.getText().isEmpty()?Integer.parseInt(epic.getText()):0;
-                grid.radius = Integer.parseInt(radius.getText());
-                if (Float.parseFloat(learnR.getText())>= 0 && Float.parseFloat(learnR.getText())<=1) {
-                    grid.learnRate = Float.parseFloat(learnR.getText());
-
-                    Alert alert = new Alert(AlertType.INFORMATION);
-                    alert.setTitle("Alert");
-                    alert.setHeaderText("Added  ");
-                    alert.showAndWait();
-                    learnRadius.setDisable(true);
-                }else {
-                    warningAlert.setHeaderText("Range should be btw 0 and 1");
-                    warningAlert.showAndWait();
-
-
-                }
-            } else {
-                warningAlert.setHeaderText("check Radius input ");
-                warningAlert.showAndWait();
-
-
-            }
-
-
-        } else {
-            warningAlert.setHeaderText(" Can't be empty ");
+        if (learnR.getText().isBlank() || radius.getText().isBlank()) {
+            warningAlert.setHeaderText("Can't be empty");
             warningAlert.showAndWait();
-
-
+            return;
         }
-
+        if (Integer.parseInt(radius.getText()) == 0) {
+            warningAlert.setHeaderText("check Radius input ");
+            warningAlert.showAndWait();
+            return;
+        }
+        grid.setEpoch(!epoch.getText().isEmpty() ? Integer.parseInt(epoch.getText()) : 0);
+        grid.setRadius(Integer.parseInt(radius.getText()));
+        if ((Float.parseFloat(learnR.getText()) < 0) || (Float.parseFloat(learnR.getText()) > 1)) {
+            warningAlert.setHeaderText("Range should be btw 0 and 1");
+            warningAlert.showAndWait();
+            return;
+        }
+        grid.setLearnRate(Float.parseFloat(learnR.getText()));
+        Alert alert = new Alert(AlertType.INFORMATION);
+        alert.setTitle(ALERT);
+        alert.setHeaderText("Added  ");
+        alert.showAndWait();
+        learnRadius.setDisable(true);
     }
 
 
-    public void findSolution(ActionEvent actionEvent) {
-
+    public void findSolution() {
         grid.solve();
-        for (int i = 0; i < grid.x; i++)
-            for (int j = 0; j < grid.y; j++)
-                pane.add(new Circle(0, 0, 4, grid.colors[i][j]), j, i); // c
+        for (int i = 0; i < grid.getX(); i++) {
+            for (int j = 0; j < grid.getY(); j++) {
+                pane.add(new Circle(0, 0, 4, grid.getColors()[i][j]), j, i); // c
+            }
+        }
 
 
     }
